a326f7bd28f98bd123090c88eebe4b55c1fe5d6b
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-04 19:00:47 +0100

    Update TestStack.java

--- src/TestStack.java
+++ src/TestStack.java
@@ -1,27 +1,18 @@
 public class TestStack {
     public static void main(String[] args) {
-        MyStack <String> myStack = new MyStack<>();
-
-
-
-                myStack.push("Nightingale");
-                myStack.push("Heron");
-                myStack.push("Tit");
-                System.out.println("myStack" + myStack.size());
-                System.out.println("myStack " + myStack.peek(0));
-                System.out.println(myStack.poll());
-
-
-                myStack.clear();
-                System.out.println("myStack " + myStack.size());
-                myStack.push("Nightingale");
-                myStack.push("Heron");
-                myStack.push("Tit");
-                System.out.println("myStack" + myStack.size());
-
-            }
-
-
-
-    }
+        MyStack<String> myStack = new MyStack<>();
+        myStack.push("Nightingale");
+        myStack.push("Heron");
+        myStack.push("Tit");
+        System.out.println("myStack" + myStack.size());
+        System.out.println("myStack " + myStack.peek(0));
+        System.out.println(myStack.poll());
+        myStack.clear();
+        System.out.println("myStack " + myStack.size());
+        myStack.push("Nightingale");
+        myStack.push("Heron");
+        myStack.push("Tit");
+        System.out.println("myStack" + myStack.size());
+   }
+}
 
