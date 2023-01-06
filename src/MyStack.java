ba01d109232f5fb0dec57c248581c9939e018cd3
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-06 20:00:57 +0100

    Update MyStack.java

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,12 +1,12 @@
 import java.util.Arrays;
 
 public class MyStack<T> implements StackInterface<T> {
-    private static int capacity = 10;
+    private static int capacity = 1;
     private Object[] array = new Object[capacity];
     private Object[] newElementRemove = new Object[capacity];
     private int size;
     private void grow() {
-        array = Arrays.copyOf(array, capacity *= 1.5);
+        array = Arrays.copyOf(array, capacity += 1);
     }
     public void push(T value) {
         if (size == capacity) {
@@ -17,21 +17,23 @@ public class MyStack<T> implements StackInterface<T> {
     }
     @Override
     public T peek() {
-        if (size() == 0)
-            return null;
-        return (T) array[size - 1];
+        if (size == 0) {
+            throw new RuntimeException("Stack is empty!");
+        }
+        return (T) array[(array.length) - 1];
     }
     @Override
     public T pop() {
         if (size == 0) {
             throw new RuntimeException("Nothing to pool! Stack is empty!");
         }
-        var lastReturn = array[0];
-        array[0] = null;
+        var lastReturn = array[size - 1];
+        array[size - 1] = null;
         size--;
         int countNewStack = 0;
+        newElementRemove = new Object[capacity];
         for (int i = 0; i < array.length; i++) {
-            if (array[i]!=null) {
+            if (array[i] != null) {
                 newElementRemove[countNewStack] = array[i];
                 countNewStack++;
             }
@@ -39,15 +41,28 @@ public class MyStack<T> implements StackInterface<T> {
         array = newElementRemove.clone();
         return (T) lastReturn;
     }
+
     @Override
     public int size() {
         return size;
     }
+
     @Override
     public void clear() {
-            array = new Object[capacity];
-            size = 0;
+        array = new Object[capacity];
+        size = 0;
+    }
+
+    @Override
+    public void printMyStack() {
+        if (size == 0) {
+            System.out.println("Stack is empty!");
+        }
+        for (int i = 0; i < size; i++) {
+            System.out.println(array[i]);
         }
     }
+}
+
 
 
