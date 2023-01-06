d7eb8eb8efe638308bf78dab751f95fd071ae2d6
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-06 16:20:09 +0100

    Update TestStack.java

--- src/TestStack.java
+++ src/TestStack.java
@@ -1,4 +1,4 @@
-public class TestStack {
+public class TestStack { 
     public static void main(String[] args) {
         MyStack<String> myStack = new MyStack<>();
         myStack.push("Nightingale");
@@ -6,13 +6,13 @@ public class TestStack {
         myStack.push("Tit");
         System.out.println("myStack" + myStack.size());
         System.out.println("myStack " + myStack.peek(0));
-        System.out.println(myStack.poll());
+        myStack.pop();
         myStack.clear();
         System.out.println("myStack " + myStack.size());
         myStack.push("Nightingale");
         myStack.push("Heron");
         myStack.push("Tit");
         System.out.println("myStack" + myStack.size());
-   }
+    }
 }
 
