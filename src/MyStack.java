0a99e261e1f20996fa2c6a97c1ba7d10a95f7e5a
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-06 16:30:02 +0100

    Update MyStack.java

--- src/MyStack.java
+++ src/MyStack.java
@@ -1,67 +1,53 @@
+import java.util.Arrays;
+
 public class MyStack<T> implements StackInterface<T> {
-    private final int INIT_SIZE = 16;
-    private final int CUT_RATE = 4;
-    private Object[] array = new Object[INIT_SIZE];
-    private int size = 0;
-    @Override
-    public void push(T element) {
-        array[size] = element;
-        System.out.println(size + "  " + array[size]);
+    private static int capacity = 10;
+    private Object[] array = new Object[capacity];
+    private Object[] newElementRemove = new Object[capacity];
+    private int size;
+    private void grow() {
+        array = Arrays.copyOf(array, capacity *= 1.5);
+    }
+    public void push(T value) {
+        if (size == capacity) {
+            grow();
+        }
+        array[size] = value;
         size++;
     }
     @Override
-    public T get(int index) {
-
-        return (T) array[index];
-    }
-    @Override
-    public T peek(int i) {
+    public T peek() {
         if (size() == 0)
             return null;
-        return (T) array[0];
+        return (T) array[size - 1];
     }
     @Override
     public T pop() {
         if (size == 0) {
-
-            return null;
+            throw new RuntimeException("Nothing to pool! Stack is empty!");
         }
+        var forReturn = array[0];
+        array[0] = null;
         size--;
-        return (T) array[size];
-    }
-    @Override
-    public T poll() {
-        if (size() == 0)
-            return null;
-        T element = (T) array[1];
-        remove(0);
-        return element;
-    }
-    @Override
-    public T remove(int index) {
-        for (int i = index; i < size; i++)
-            array[i] = array[i + 1];
-        array[size] = null;
-        size--;
-        if (array.length > INIT_SIZE && size < array.length / CUT_RATE)
-            resize(array.length / 2);
-
-        return null;
+        int countNewStack = 0;
+        for (int i = 0; i < array.length; i++) {
+            if (array[i]!=null) {
+                newElementRemove[countNewStack] = array[i];
+                countNewStack++;
+            }
+        }
+        array = newElementRemove.clone();
+        return (T) forReturn;
     }
     @Override
     public int size() {
         return size;
     }
-
     @Override
     public void clear() {
         for (int i = 0; i <= size; i++)
             array[size] = null;
         size = 0;
     }
-    private void resize(int newLength) {
-        Object[] newArray = new Object[newLength];
-        System.arraycopy(array, 0, newArray, 0, size);
-        array = newArray;
-    }
+
 }
