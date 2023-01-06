61870fde3838e7e9c2637599fa4c0f19bd69e081
Artem Knyzhnykov <111915140+Zelenyj2R@users.noreply.github.com>
2023-01-06 16:19:08 +0100

    Update StackInterface.java

--- src/StackInterface.java
+++ src/StackInterface.java
@@ -1,11 +1,7 @@
 public interface StackInterface <T> {
-
     void push(T element);
-    T get(int index);
     T peek(int i);
     T pop();
-    T poll();
-    T remove(int index);
     int size();
     void clear();
 }
